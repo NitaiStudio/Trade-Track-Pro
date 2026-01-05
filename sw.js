c0ee0db8f2cb0c68408332e9a6f137d7a1276b16
Nitai Sarkar <140227933+NitaiStudio@users.noreply.github.com>
2026-01-06 02:18:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE_NAME = 'tt-pro-v2';
-const assets = [
-  './',
-  './index.html',
-  'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap'
-];
+const CACHE_NAME = 'tradetrack-v5';
+const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
